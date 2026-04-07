d7d7c1fafbd856c82440b0f4d6869960df38bdc1
yhalleez1 <yhalleez@gmail.com>
2026-04-07 08:24:50 +0300

    Update SMS API payload and header values

--- projec/dqms/mobi.py
+++ projec/dqms/mobi.py
@@ -3,16 +3,16 @@ import requests
 url = "https://app.mobitechtechnologies.com/sms/sendsms"
 
 headers = {
-    "h_api_key": "4575d6f769777051dde88e8f1987bcf2d9a94cf5e649d3798b5f3f6dc6868d5c",
+    "h_api_key": "4575d6f769777051dde88e3798b5f3f6dc6868d5c",
     "Content-Type": "application/json"
 }
 
 payload = {
-    "mobile": "+254700753710",
+    "mobile": "+25470",
     "response_type": "json",
-    "sender_name": "FULL_CIRCLE",
+    "sender_name": "FURCLE",
     "service_id": 0,
-    "message": "Hello Suro, just testing for SMS working!"
+    "message": "Hello, just testing for SMS working!"
 }
 
 response = requests.post(url, headers=headers, json=payload)
